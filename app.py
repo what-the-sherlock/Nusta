9112336d8ce65725917ddfecd3848f447515d59a
what-the-sherlock <119885261+what-the-sherlock@users.noreply.github.com>
2024-09-08 14:00:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("OHRC images")
 st.markdown("1.")
 
 image_comparison(
-    img1="https://www.webbcompare.com/img/hubble/southern_nebula_700.jpg",
+    img1="https://github.com/what-the-sherlock/Nusta/blob/25eec79cacb85936b567e56bbaad2b168ebc6c19/WhatsApp%20Image%202024-09-08%20at%2013.50.28_b4fa08cc.jpg",
     img2="https://www.webbcompare.com/img/webb/southern_nebula_700.jpg",
     label1="Original",
     label2="Enhanced"
